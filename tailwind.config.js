a37fe342dbda22b58430514f1e98c816da30eaf8
Yehuda Neufeld <newgraphil@gmail.com>
2020-08-30 01:52:15 +0300

    initial design implementation

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './*.html',
+    './**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundOpacity: ['responsive', 'hover', 'group-hover'],
+  },
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ],
+}
